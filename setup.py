5da43b0a408b5c63e28d9fc8442fdeec0807b2b8
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2019-01-24 19:43:59 +0100

    Added package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dataming',
+      version='0.1',
+      description='Streaming dataset simulator',
+      url='https://github.com/danhidsan/dataming',
+      author='Daniel Hidalgo',
+      author_email='dhidalgosanchez96@gmail.com',
+      license='MIT',
+      packages=['dataming'],
+      install_requires=['pandas'],
+      zip_safe=False)
