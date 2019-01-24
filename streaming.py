ec65747c092ee790cbcdc025a29a2cbd716de932
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2019-01-24 17:49:44 +0100

    Added streaming for JSON files

--- streaming.py
+++ streaming.py
@@ -1,7 +1,6 @@
 
 import pandas as pd
 import time
-import sys
 import re
 
 
@@ -48,15 +47,15 @@ class StreamingSimulatorCsv(StreamingSimulator):
     def __read_file(self):
 
         try:
-            readed_csv = pd.read_csv(
+            csv = pd.read_csv(
                 self.file_path,
                 sep=self.sep,
                 delimiter=self.delimiter,
                 delim_whitespace=self.delim_whitespace)
 
-            return readed_csv
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
+            return csv
+        except Exception as e:
+            print(e)
 
     def data_head(self, lines=5):
         """Return head data from params lines
@@ -85,4 +84,64 @@ class StreamingSimulatorCsv(StreamingSimulator):
             time.sleep(self.lapse)
             on_simulate(rows_values[index:index+window])
 
+
+class StreamingSimulatorJSON(StreamingSimulator):
+
+    def __init__(self, file_path, lapse=1, data_window=1, lines=False):
+
+        # Parent class
+        StreamingSimulator.__init__(self, file_path, lapse, data_window)
+
+        # Checks
+        self.check_json(file_path)
+
+        # Attributes
+        self.lines = lines
+        self.__data = self.__read_file
+
+    @staticmethod
+    def check_json(file_path):
+        """Check if file from file path, is JSON file
+
+               Params:
+                   file_path -- File path string
+
+               Exceptions:
+                   Exception -- If file from file path is not JSON file
+               """
+        search = re.search('.json', file_path)
+
+        if search is None:
+            raise Exception("File path must be JSON file")
+
+    @property
+    def __read_file(self):
+
+        try:
+            json = pd.read_json(self.file_path, typ='series', lines=self.lines)
+            return json
+        except Exception as e:
+            print('Unexpected error: ')
+            print(e)
+
+    def simulate(self, on_simulate=print):
+        """Simulate streaming data flow.
+
+                Params:
+
+                    on_simulate -- function callback to define row by row action
+        """
+        shapes = self.__data.shape
+        print("Simulating %d rows..." % (shapes[0],))
+
+        rows_values = self.__data
+        len_data = len(rows_values)
+        window = self.data_window
+
+        for index in range(0, len_data, window):
+            time.sleep(self.lapse)
+            dict_to_list = []
+            for key, value in rows_values[index].items():
+                dict_to_list.append((key, value))
+            on_simulate(dict_to_list)
 
